0cebbc6fc6a0af54f1a66f616aaf32bb2accca24
electrototo <cristobal@liendo.net>
2018-01-18 08:38:47 -0600

    Change structs to bidimensional arrays (idk, requirements for the class)

--- ejercicios/alumnos.c
+++ ejercicios/alumnos.c
@@ -6,53 +6,84 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct alumno {
-    char nombre[100];
-    float calificaciones[3];
-} alumno_t;
+#include <string.h>
 
 int menu_principal();
-void preguntar_datos(alumno_t *);
+void preguntar_datos(int a_index, char **nombres, float (*calificaciones)[3]);
 void quitar_enter(char *);
-void guardar_en_archivo(alumno_t *);
-void leer_datos_archivo(alumno_t *);
-void mostrar_datos_alumno(alumno_t *);
-void promedios_alumnos(alumno_t *);
+void guardar_en_archivo(char **nombres, float (*calificaciones)[3]);
+void leer_datos_archivo(char **nombres, float (*calificaciones)[3]);
+void mostrar_datos_alumno(char **nombres, float (*calificaciones)[3]);
+void promedios_alumnos(char **nombres, float (*calificaciones)[3]);
 FILE *get_file(char *mode);
 
 int main() {
-    int choice;
-    alumno_t alumnos[10];
+    int choice, data_in = 0;
+
+    char *nombres[10];
+    float calificaciones[10][3];
 
     while ((choice = menu_principal()) != 6) {
         switch (choice) {
             case 1:
                 for (int i = 0; i < 10; i++)
-                    preguntar_datos(&alumnos[i]);
+                    preguntar_datos(i, nombres, calificaciones);
+
+                data_in = 1;
 
                 break;
 
             case 2:
-                guardar_en_archivo(alumnos);
+                if (data_in)
+                    guardar_en_archivo(nombres, calificaciones);
+
+                else {
+                    printf("No hay informacion en memoria\n");
+                    printf("\nPresiona cualquier tecla para continuar...");
+                    getchar();
+                }
+
+
                 break;
 
             case 3:
-                leer_datos_archivo(alumnos);
+                leer_datos_archivo(nombres, calificaciones);
+                data_in = 1;
+
                 break;
 
             case 4:
-                mostrar_datos_alumno(alumnos);
+                if (data_in)
+                    mostrar_datos_alumno(nombres, calificaciones);
+
+                else {
+                    printf("No hay informacion en memoria\n");
+                    printf("\nPresiona cualquier tecla para continuar...");
+                    getchar();
+                }
+
                 break;
 
             case 5:
-                promedios_alumnos(alumnos);
+                if (data_in)
+                    promedios_alumnos(nombres, calificaciones);
+
+                else {
+                    printf("No hay informacion en memoria\n");
+                    printf("\nPresiona cualquier tecla para continuar...");
+                    getchar();
+                }
+
                 break;
 
             default:
                 break;
         }
     }
+
+    if (data_in)
+        for (int i = 0; i < 10; i++)
+            free(nombres[i]);
 
     return 0;
 }
@@ -62,7 +93,7 @@ int menu_principal() {
 
     int choice;
 
-    printf("\nQue deseas hacer?\n");
+    printf("Que deseas hacer?\n");
     printf("[1] Leer datos de alumnos\n");
     printf("[2] Guardar en archivo\n");
     printf("[3] Leer datos de archivo\n");
@@ -87,14 +118,18 @@ void quitar_enter(char *name) {
     *name = '\0';
 }
 
-void preguntar_datos(alumno_t *alumno) {
+void preguntar_datos(int a_index, char **nombres, float (*calificaciones)[3]) {
+    char tmp[200];
+
     printf("\nIngresa el nombre del alumno: ");
-    fgets(alumno->nombre, 199, stdin);
-    quitar_enter(alumno->nombre);
+    fgets(tmp, 199, stdin);
+    quitar_enter(tmp);
+
+    nombres[a_index] = strdup(tmp);
 
     for (int i = 0; i < 3; i++) {
         printf("\tIngresa la calificacion #%d: ", i + 1);
-        scanf("%f", &alumno->calificaciones[i]);
+        scanf("%f", &calificaciones[a_index][i]);
         getchar();
     }
 }
@@ -115,14 +150,14 @@ FILE *get_file(char *mode) {
     return file;
 }
 
-void guardar_en_archivo(alumno_t *alumnos) {
+void guardar_en_archivo(char **nombres, float (*calificaciones)[3]) {
     FILE *file = get_file("w");
 
     for (int i = 0; i < 10; i++) {
-        fprintf(file, "%s\n", alumnos[i].nombre);
-        fprintf(file, "%f\n", alumnos[i].calificaciones[0]);
-        fprintf(file, "%f\n", alumnos[i].calificaciones[1]);
-        fprintf(file, "%f\n", alumnos[i].calificaciones[2]);
+        fprintf(file, "%s\n", nombres[i]);
+        fprintf(file, "%f\n", calificaciones[i][0]);
+        fprintf(file, "%f\n", calificaciones[i][1]);
+        fprintf(file, "%f\n", calificaciones[i][2]);
     }
 
     printf("\tSe guardaron exitosamente las calificaciones de los alumnos\n");
@@ -133,23 +168,27 @@ void guardar_en_archivo(alumno_t *alumnos) {
     fclose(file);
 }
 
-void imprimir_dato_alumno(alumno_t *alumno) {
-    printf("\tNombre del alumno: %s\n", alumno->nombre);
-    printf("\t  Calificacion #1: %.2f\n", alumno->calificaciones[0]);
-    printf("\t  Calificacion #2: %.2f\n", alumno->calificaciones[1]);
-    printf("\t  Calificacion #3: %.2f\n\n", alumno->calificaciones[2]);
+void imprimir_dato_alumno(int a_index, char **nombres, float (*calificaciones)[3]) {
+    printf("\tNombre del alumno: %s\n", nombres[a_index]);
+    printf("\t  Calificacion #1: %.2f\n", calificaciones[a_index][0]);
+    printf("\t  Calificacion #2: %.2f\n", calificaciones[a_index][1]);
+    printf("\t  Calificacion #3: %.2f\n\n", calificaciones[a_index][2]);
 }
 
-void leer_datos_archivo(alumno_t *alumnos) {
+void leer_datos_archivo(char **nombres, float (*calificaciones)[3]) {
     FILE *file = get_file("r");
+    char temp[200];
 
     for (int i = 0; i < 10; i++) {
-        fscanf(file, "%s\n", alumnos[i].nombre);
-        fscanf(file, "%f\n", &alumnos[i].calificaciones[0]);
-        fscanf(file, "%f\n", &alumnos[i].calificaciones[1]);
-        fscanf(file, "%f\n", &alumnos[i].calificaciones[2]);
+        fscanf(file, "%s\n", temp);
+        fscanf(file, "%f\n", &calificaciones[i][0]);
+        fscanf(file, "%f\n", &calificaciones[i][1]);
+        fscanf(file, "%f\n", &calificaciones[i][2]);
 
-        imprimir_dato_alumno(&alumnos[i]);
+        nombres[i] = strdup(temp);
+        memset(temp, 0, 200);
+
+        imprimir_dato_alumno(i, nombres, calificaciones);
     }
 
     printf("Presione enter para continuar...");
@@ -158,22 +197,22 @@ void leer_datos_archivo(alumno_t *alumnos) {
     fclose(file);
 }
 
-void mostrar_datos_alumno(alumno_t * alumnos) {
+void mostrar_datos_alumno(char **nombres, float (*calificaciones)[3]) {
     for (int i = 0; i < 10; i++)
-        imprimir_dato_alumno(&alumnos[i]);
+        imprimir_dato_alumno(i, nombres, calificaciones);
 
     printf("Presione enter para continuar...");
     getchar();
 }
 
-void promedios_alumnos(alumno_t *alumnos) {
+void promedios_alumnos(char **nombres, float (*calificaciones)[3]) {
     float avg = 0;
 
     for (int i = 0; i < 10; i++) {
         for(int j = 0; j < 3; j++)
-            avg += alumnos[i].calificaciones[j] / 3;
+            avg += calificaciones[i][j] / 3;
 
-        printf("\tAlumno %s:\n", alumnos[i].nombre);
+        printf("\tAlumno %s:\n", nombres[i]);
         printf("\t  Promedio %.2f\n\n", avg);
 
         avg = 0;
