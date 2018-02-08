bfa17b15aa636c41c3bc8345442f9dd527d6d57e
electrototo <cristobal@liendo.net>
2018-02-07 19:59:10 -0600

    Rename files

---
+++ laboratorio/Practica2/practica2.c
@@ -0,0 +1,13 @@
+/*
+ * autor: cristobal liendo i
+ * fecha: Tue 06 Feb 2018 05:10:24 PM CST
+ * descripcion: Genera un arbol de huffman, comprimiendo el archivo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    FILE 
+    return 0;
+}
