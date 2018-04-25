a9873b91aa9253f93769afccbefe958f8885e1e5
electrototo <cristobal@liendo.net>
2018-04-25 18:52:07 -0500

    Agregar codigo para pente

---
+++ laboratorio/Pente/pente.c
@@ -0,0 +1,13 @@
+/*
+ * autores: estibaliz cano l, isabel leon s, cristobal liendo i
+ * fecha: Tue 24 Apr 2018 05:51:29 PM CDT
+ * descripcion: programa del proyecto final de pente
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
+
