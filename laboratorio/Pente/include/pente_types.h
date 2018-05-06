6c17d61404ba31423009f7c8a5b2c2e4bf1fe8a1
electrototo <cristobal@liendo.net>
2018-05-06 11:07:26 -0500

    Add struct for simplifying menu creation

--- laboratorio/Pente/include/pente_types.h
+++ laboratorio/Pente/include/pente_types.h
@@ -3,6 +3,10 @@
  * Escrito por Cristobal Liendo <cristobal@liendo.net>
 */
 
+
+#ifndef PTYPESH
+#define PTYPESH
+
 typedef struct image_data_t{
     GtkWidget *image;
     int x, y;
@@ -15,3 +19,11 @@ typedef struct game_info_t {
     int player1_p, player2_p;
     int ccount_1, ccount_2;
 } game_info_t;
+
+typedef struct menu_item_dt {
+    char label[15];
+    void (*callback)(GtkMenuItem *, gpointer);
+    gpointer data;
+} menu_item_dt;
+
+#endif
